e382273b45c99f2f2add9e9b0b7b9c4d8c171c6d
constneo <nqxkk@qq.com>
2022-01-06 15:33:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from 'https://deno.land/std@0.114.0/http/server.ts'
 
 function handler(req) {
-  return new Response('Hello world,WangLijie')
+  return new Response('Hello world,SBFei')
 }
 
 console.log('Listening on http://localhost:8000')
